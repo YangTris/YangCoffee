077da4a8d80e7410e44d1ba249244bdf116c834d
Dương Văn Trí <id.dvantri911@gmail.com>
2025-04-14 14:39:16 +0700

    Create Category.cs

---
+++ Domain/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class Category
+    {
+        public string CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual List<Product> Products { get; set; }
+    }
+}
