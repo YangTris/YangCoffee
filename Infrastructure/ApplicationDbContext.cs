54f2f85cd5de80b184ac6ae8726dfa899bc48b47
Dương Văn Trí <id.dvantri911@gmail.com>
2025-04-15 11:55:05 +0700

    Update ApplicationDbContext.cs

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -12,15 +12,15 @@ namespace Infrastructure
 {
     public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
-        DbSet<Cart> Carts { get; set; }
-        DbSet<CartItem> CartItems { get; set; }
-        DbSet<Category> Categories { get; set; }
-        DbSet<Order> Orders { get; set; }
-        DbSet<OrderDetail> OrderDetails { get; set; }
-        DbSet<Product> Products { get; set; }
-        DbSet<ProductImage> ProductImages { get; set; }
-        DbSet<ProductVariant> ProductVariants { get; set; }
-        DbSet<User> Users { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<ProductVariant> ProductVariants { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
