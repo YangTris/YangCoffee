50281f1ac95b4d7e41b73d595675eb929cd9662d
Dương Văn Trí <id.dvantri911@gmail.com>
2025-04-15 11:54:48 +0700

    Add: Category Repository

---
+++ Application/IRepositories/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Application.IRepositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(string id);
+        Task<Category> AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(string id);
+    }
+}
---
+++ Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs;
+using Application.IRepositories;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category> AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(string id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
