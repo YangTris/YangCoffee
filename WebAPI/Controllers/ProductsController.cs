8aa1128d06acb9612f02274a30dc0285d0e50efc
Dương Văn Trí <id.dvantri911@gmail.com>
2025-04-19 21:46:44 +0700

    Add: Product Controller

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,149 @@
+﻿using Application.DTOs;
+using Application.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetAll()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ProductDTO>> GetById(string id)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Add([FromBody] ProductDTO productDTO)
+        {
+            if (productDTO == null)
+            {
+                return BadRequest();
+            }
+            var product = await _productService.AddProductAsync(productDTO);
+            return CreatedAtAction(nameof(GetById), new { id = product.ProductId }, product);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update(string id, [FromBody] UpdateProductDTO updateProductDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var existingProduct = await _productService.GetProductByIdAsync(id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            await _productService.UpdateProductAsync(id, updateProductDTO);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            await _productService.DeleteProductAsync(id);
+            return NoContent();
+        }
+
+        [HttpGet("variant/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ProductVariantDTO>> GetProductVariantById(string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var productVariant = await _productService.GetProductVariantByIdAsync(id);
+            if (productVariant == null)
+            {
+                return NotFound();
+            }
+            return Ok(productVariant);
+        }
+
+        [HttpPost("variant/{productId}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> AddProductVariant(string productId, [FromBody] ProductVariantDTO productVariantDTO)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var existingProduct = await _productService.GetProductByIdAsync(productId);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            var productVariant = await _productService.AddProductVariantAsync(productId, productVariantDTO);
+            return CreatedAtAction(nameof(GetProductVariantById), new { id = productVariant.ProductVariantId }, productVariant);
+        }
+
+        [HttpPut("variant/{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateProductVariant(string id, [FromBody] ProductVariantDTO productVariantDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var existingProductVariant = await _productService.GetProductVariantByIdAsync(id);
+            if (existingProductVariant == null)
+            {
+                return NotFound();
+            }
+            await _productService.UpdateProductVariantAsync(id, productVariantDTO);
+            return NoContent();
+        }
+    }
+}
