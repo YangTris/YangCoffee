bb412cb40268db4125a7da01f4991e60dddebc39
Dương Văn Trí <id.dvantri911@gmail.com>
2025-04-27 17:17:48 +0700

    Update Order.cs

--- Domain/Order.cs
+++ Domain/Order.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Enum;
 
 namespace Domain
 {
@@ -6,9 +8,14 @@ namespace Domain
     {
         [Key]
         public string OrderId { get; set; }
-        public string Address { get; set; }
-        public string Status { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+        public string ShippingAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public OrderStatus OrderStatus { get; set; }
         public DateTimeOffset OrderDate { get; set; }
         public decimal TotalAmount { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
     }
 }
