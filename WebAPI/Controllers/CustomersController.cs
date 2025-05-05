8e12c72ef0bf5419e07ffaa8839b5ad840cf8780
Dương Văn Trí <id.dvantri911@gmail.com>
2025-05-05 09:50:40 +0700

    Create CustomersController.cs

---
+++ WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController(UserManager<IdentityUser> userManager) : ControllerBase
+    {
+        private UserManager<IdentityUser> _userManager = userManager;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUser()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return Ok(users);
+        }
+    }
+}
