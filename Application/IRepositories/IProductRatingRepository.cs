aa04c6b3c70d3bf9ab101bda4793d7ec987446c7
Dương Văn Trí <id.dvantri911@gmail.com>
2025-05-07 10:48:06 +0700

    Add: ProductRating Repository

---
+++ Application/IRepositories/IProductRatingRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Application.IRepositories
+{
+    public interface IProductRatingRepository
+    {
+        Task<ProductRating> GetUserRatingForProductAsync(string userId, string productId);
+        Task AddAsync(ProductRating rating);
+    }
+}
