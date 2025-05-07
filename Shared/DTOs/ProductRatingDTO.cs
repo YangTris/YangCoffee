ac568e5c83007ca2fa4c6e84f49bfcf9cc8f7688
Dương Văn Trí <id.dvantri911@gmail.com>
2025-05-07 10:49:20 +0700

    Add: ProductRating Controller

---
+++ Shared/DTOs/ProductRatingDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Shared.DTOs
+{
+    public class ProductRatingDTO
+    {
+        public string ProductRatingId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public string UserId { get; set; }
+        public string ProductId { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/ProductRatingsController.cs
@@ -0,0 +1,43 @@
+﻿using Application.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTOs;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductRatingsController : ControllerBase
+    {
+        private readonly IProductRatingService _productRatingService;
+        public ProductRatingsController(IProductRatingService productRatingService)
+        {
+            _productRatingService = productRatingService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public async Task<ActionResult<ProductRatingDTO>> RateProduct([FromBody] ProductRatingDTO productRatingDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _productRatingService.RateProductAsync(
+                productRatingDto.UserId,
+                productRatingDto.ProductId,
+                productRatingDto.Rating,
+                productRatingDto.Comment);
+
+            if (result == null)
+            {
+                return Conflict("You have already rated this product.");
+            }
+
+            return CreatedAtAction(nameof(RateProduct), new { id = result.ProductRatingId }, result);
+        }
+    }
+}
