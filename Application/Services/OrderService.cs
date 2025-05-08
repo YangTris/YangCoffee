e3b91a5252a7c425216f3158e600d8a883be762c
Dương Văn Trí <id.dvantri911@gmail.com>
2025-05-08 15:38:14 +0700

    Update OrderStatus code is Susscess

--- Application/Services/OrderService.cs
+++ Application/Services/OrderService.cs
@@ -66,7 +66,7 @@ namespace Application.Services
                 UserId = userId,
                 TotalAmount = cart.CartItems.Sum(ci => ci.Quantity * ci.Price),
                 OrderDate = DateTimeOffset.Now,
-                OrderStatus = OrderStatus.Processing,
+                OrderStatus = OrderStatus.Success,
                 ShippingAddress = shippingAddress,
                 PhoneNumber = phoneNumber
             };
